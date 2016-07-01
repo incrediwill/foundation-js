b135f5837d15129cb3891df02cd35548332902c2
Will <william.elder@ebizautos.com>
2016-07-01 10:09:58 -0700

    Update foundation-beta.js
    Updated disclaimer function

--- foundation-beta.js
+++ foundation-beta.js
@@ -2971,28 +2971,23 @@ function CalculatorPopulateField(obj, defaultValue) {
             });
         }
     
-    // Slide Disclaimer    
-	var $sliderDisclaimer = $('.slide-disclaimer-btn');
+	// Slide Disclaimer    
+	var elementOpenText = $('.slide-disclaimer-btn').attr('data-open-text');
+	var elementCloseText = $('.slide-disclaimer-btn').attr('data-close-text');
 	
-		$sliderDisclaimer.each(function() {
-			var element = $(this),
-				elementOpenText = element.attr('data-open-text'),
-				elementCloseText = element.attr('data-close-text');
-			var is_visible = false;
-			
-			$('.slide-disclaimer').prev('.slider-disclaimer-btn').append('<a href="#" class="slide-disclaimer-link">'+$elementOpenText+'</a>');
-			
-			$('.slide-disclaimer').hide();
-			
-			$('.slide-disclaimer-link').click(function() {
-				 
-			    $(this).html( ($(this).parent().next('.slide-disclaimer').is(":visible")) ? $elementOpenText : $elementCloseText);
-			
-				$(this).parent().next('.slide-disclaimer').toggle();
-				 
-				return false;
-				 
-			});
+	console.log (elementOpenText,elementCloseText);
 	
-		});
+	$('.slide-disclaimer-btn').append('<a href="#" class="slide-disclaimer-link">'+elementOpenText+'</a>');
+	
+	$('.slide-disclaimer').hide();
+	
+	$('.slide-disclaimer-link').click(function() {
+		 
+	    $(this).html( ($(this).parent().next('.slide-disclaimer').is(":visible")) ? elementOpenText : elementCloseText);
+	
+		$(this).parent().next('.slide-disclaimer').toggle();
+		 
+		return false;
+		 
+	});
 })(jQuery);
